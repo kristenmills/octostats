44713abac433dcd47923dc7bbea4a0ac4cc44837
kristenmills <kristen@kristen-mills.com>
2013-08-12 00:40:50 -0700

    Added high charts and the repositories by language pie chart

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -3,4 +3,13 @@ class SharedHelper
     time = Time.parse(client.last_response.headers['date'])
     time.strftime('Last Updated: %B %d, %Y %R %Z')
   end
+
+  def self.to_data hash
+    array_string = ''
+    hash.each do |key, value|
+      array_string += "['#{key or 'None'}', #{value}],"
+    end
+    array_string.chop!
+    "[#{array_string}]"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User
 
-  attr_reader :user, :repos
+  attr_reader :user, :repos, :languages, :events
   # Creates a new user object based on user events
   #
   # @param username [String] the username that you are looking at
@@ -9,13 +9,14 @@ class User
     @client = client
     @user = @client.user(username)
     @repos = @client.repos(username)
-    process @client.user_public_events(username)
+    process_events @client.user_public_events(username)
+    language_dist
   end
 
   # Processes the events so that the data is useful to us
   #
   # @params events [Array] and array of event objects.
-  def process events
+  def process_events events
     @events = Hash.new
 
     # Organize the event based on type
@@ -23,6 +24,15 @@ class User
     events.each do |event|
       @events[event.type] ||= Array.new
       @events[event.type] << event
+    end
+  end
+
+  # Goes through the languages and makes a hash of how many repos you have
+  # per language
+  def language_dist
+    @languages = Hash.new(0)
+    @repos.each do |repo|
+      @languages[repo.language] += 1
     end
   end
 
